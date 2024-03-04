a5030c193b23cdf8087a1bf5032d6c5f1f921212
Joshua Almeida <joshuajamcraft@gmail.com>
2024-03-03 22:16:11 -0300

    literalmente pq caralhos algm usaria portugol nmrl

---
+++ nomeidadedois.py
@@ -0,0 +1,21 @@
+impar1 = "Josma"
+par1 = 19
+impar2 = "Momoster"
+par2 = 18
+impar3 = "Cabecudo"
+par3 = 21
+impar4 = "Egoista"
+par4 = 22
+impar5 = "Marry"
+par5 = 21
+impar6 = "Lipesena"
+par6 = 21
+somapares = par1 + par2 + par3 + par4 + par5 + par6
+print(impar1, par1)
+print(impar2, par2)
+print(impar3, par3)
+print(impar4, par4)
+print(impar5, par5)
+print(impar6, par6)
+print(somapares)
+print(somapares / 6)
